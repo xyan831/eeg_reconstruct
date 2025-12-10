bc6207fd636792ea52b1c5f2b35d034f2f2bbd76
xy83-uw <xy83@uw.edu>
2025-12-10 11:22:13 -0800

    update

--- scripts/model_unet.py
+++ scripts/model_unet.py
@@ -27,17 +27,14 @@ class UNet1D(nn.Module):
         self.outc = nn.Conv1d(64, out_channels, kernel_size=1)
 
     def forward(self, x):
-        # Encoder
         x1 = self.inc(x)
         x2 = self.down1(x1)
         x3 = self.down2(x2)
         x4 = self.down3(x3)
         x5 = self.down4(x4)
 
-        # Bottleneck
         x6 = self.bottleneck(x5)
 
-        # Decoder
         x = self.up1(x6, x4)
         x = self.up2(x, x3)
         x = self.up3(x, x2)
@@ -45,65 +42,52 @@ class UNet1D(nn.Module):
 
         return self.outc(x)
 
+# DoubleConv
 class DoubleConv(nn.Module):
-    """(convolution => [BN] => ReLU) * 2"""
-    
-    def __init__(self, in_channels, out_channels, mid_channels=None):
-        super().__init__()
-        if not mid_channels:
-            mid_channels = out_channels
-        self.double_conv = nn.Sequential(
-            nn.Conv1d(in_channels, mid_channels, kernel_size=3, padding=1),
-            nn.BatchNorm1d(mid_channels),
+    def __init__(self, in_ch, out_ch):
+        super(DoubleConv, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv1d(in_ch, out_ch, kernel_size=3, padding=1),
+            nn.BatchNorm1d(out_ch),
             nn.ReLU(inplace=True),
-            nn.Conv1d(mid_channels, out_channels, kernel_size=3, padding=1),
-            nn.BatchNorm1d(out_channels),
+            nn.Conv1d(out_ch, out_ch, kernel_size=3, padding=1),
+            nn.BatchNorm1d(out_ch),
             nn.ReLU(inplace=True)
         )
+        #self.attn = AttentionBlock1D(out_ch)
 
     def forward(self, x):
-        return self.double_conv(x)
+        x = self.conv(x)
+        #x = self.attn(x)
+        return x
 
+# Downsampling block
 class Down(nn.Module):
-    """Downscaling with maxpool then double conv"""
-    
-    def __init__(self, in_channels, out_channels):
-        super().__init__()
-        self.maxpool_conv = nn.Sequential(
+    def __init__(self, in_ch, out_ch):
+        super(Down, self).__init__()
+        self.down = nn.Sequential(
             nn.MaxPool1d(2),
-            DoubleConv(in_channels, out_channels)
+            DoubleConv(in_ch, out_ch)
         )
 
     def forward(self, x):
-        return self.maxpool_conv(x)
+        return self.down(x)
 
+# Upsampling block
 class Up(nn.Module):
-    """Upscaling then double conv"""
-    
-    def __init__(self, in_channels, out_channels, bilinear=True):
-        super().__init__()
-        
-        # if bilinear, use the normal convolutions to reduce the number of channels
-        if bilinear:
-            self.up = nn.Upsample(scale_factor=2, mode='linear', align_corners=True)
-            self.conv = DoubleConv(in_channels, out_channels, in_channels // 2)
-        else:
-            self.up = nn.ConvTranspose1d(in_channels, in_channels // 2, kernel_size=2, stride=2)
-            self.conv = DoubleConv(in_channels, out_channels)
+    def __init__(self, in_ch, out_ch):
+        super(Up, self).__init__()
+        self.up = nn.Upsample(scale_factor=2, mode='linear', align_corners=True)
+        self.conv = DoubleConv(in_ch, out_ch)
 
     def forward(self, x1, x2):
         x1 = self.up(x1)
-        
-        # input is CHW
-        diff = x2.size()[2] - x1.size()[2]
-        
-        # Pad if necessary
+        # Pad if size mismatch due to rounding
+        diff = x2.size(-1) - x1.size(-1)
         if diff > 0:
-            x1 = F.pad(x1, [diff // 2, diff - diff // 2])
+            x1 = F.pad(x1, (0, diff))
         elif diff < 0:
-            x2 = F.pad(x2, [-diff // 2, -(-diff // 2)])
-        
-        # Concatenate along channel dimension
+            x2 = F.pad(x2, (0, -diff))
         x = torch.cat([x2, x1], dim=1)
         return self.conv(x)
 
