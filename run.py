d8c6410a0abf934299c74bba7f91563ba9a4788c
Xiaochen Yan <otheranon5322@gmail.com>
2026-01-21 15:34:12 -0800

    update

--- run.py
+++ run.py
@@ -2,26 +2,28 @@ import os
 
 from scripts.test_functions import run_data, run_mat, run_recon, run_class
 
-def prepare_rawdata(path_config):
-    # our dataset
+def prepare_rawdata_our(path_config):
     run_data(path_config, "our", 26, "our01", r'eeg_0[1-9]\.edf|eeg_10\.edf')
     run_data(path_config, "our", 26, "our02", r'eeg_1[1-9]\.edf|eeg_20\.edf')
     run_data(path_config, "our", 26, "our03", r'eeg_2[1-9]\.edf|eeg_30\.edf')
     run_data(path_config, "our", 26, "our04", r'eeg_3[1-9]\.edf|eeg_40\.edf')
     run_data(path_config, "our", 26, "our05", r'eeg_4[1-9]\.edf|eeg_50\.edf')
     run_data(path_config, "our", 26, "our06", r'eeg_5[1-9]\.edf|eeg_60\.edf')
-    run_data(path_config, "our", 26, "our07", r'eeg_6[1-9]\.edf|eeg_70\.edf')
-    run_data(path_config, "our", 26, "our08", r'eeg_7[1-9]\.edf|eeg_80\.edf')
-    run_data(path_config, "our", 26, "our09", r'eeg_8[1-9]\.edf|eeg_90\.edf')
-    run_data(path_config, "our", 26, "our10", r'eeg_9[1-9]\.edf|eeg_100\.edf')
-    # nicu dataset
+    #run_data(path_config, "our", 26, "our07", r'eeg_6[1-9]\.edf|eeg_70\.edf')
+    #run_data(path_config, "our", 26, "our08", r'eeg_7[1-9]\.edf|eeg_80\.edf')
+    #run_data(path_config, "our", 26, "our09", r'eeg_8[1-9]\.edf|eeg_90\.edf')
+    #run_data(path_config, "our", 26, "our10", r'eeg_9[1-9]\.edf|eeg_100\.edf')
+
+def prepare_rawdata_nicu(path_config):
     run_data(path_config, "nicu", 21, "nicu01", r'eeg_0[1-9]\.edf|eeg_10\.edf')
     run_data(path_config, "nicu", 21, "nicu02", r'eeg_1[1-9]\.edf|eeg_20\.edf')
 
-def prepare_dataset(path_config, ch_max, block_ch, is_custom, name_ch):
+def prepare_dataset_our(path_config, ch_max, block_ch, is_custom, name_ch):
     run_mat(path_config, "both", ch_max, block_ch, is_custom, f"our{name_ch}01", ["our01","our02","our03"])
     run_mat(path_config, "seiz", ch_max, block_ch, is_custom, f"our{name_ch}02", ["our04","our05","our06"])
     run_mat(path_config, "nseiz", ch_max, block_ch, is_custom, f"our{name_ch}02", ["our04","our05","our06"])
+
+def prepare_dataset_nicu(path_config, ch_max, block_ch, is_custom, name_ch):
     run_mat(path_config, "both", ch_max, block_ch, is_custom, f"nicu{name_ch}01", ["nicu01"])
     run_mat(path_config, "seiz", ch_max, block_ch, is_custom, f"nicu{name_ch}02", ["nicu02"])
     run_mat(path_config, "nseiz", ch_max, block_ch, is_custom, f"nicu{name_ch}02", ["nicu02"])
@@ -140,27 +142,25 @@ if __name__ == "__main__":
         "raw_path": raw_path
     }
     
-    #prepare_rawdata(path_config)
+    #prepare_rawdata_our(path_config)
+    #prepare_rawdata_nicu(path_config)
     
-    #prepare_dataset(path_config, ch_max, block_ch, is_custom, name_ch)
+    prepare_dataset_our(path_config, ch_max, block_ch, is_custom, name_ch)
+    #prepare_dataset_nicu(path_config, ch_max, block_ch, is_custom, name_ch)
     
-    #recon_train(path_config, "our", our_data1, our_recon_model, epoch, learn_rate, isFFT, savebest, makevis, sample)
+    recon_train(path_config, "our", our_data1, our_recon_model, epoch, learn_rate, isFFT, savebest, makevis, sample)
     #recon_train(path_config, "nicu", nicu_data1, nicu_recon_model, epoch, learn_rate, isFFT, savebest, makevis, sample)
     
-    #recon_test(path_config, our_data2, our_recon_model, epoch, learn_rate, isFFT, savebest, makevis, sample):
-    #recon_test(path_config, nicu_data2, nicu_recon_model, epoch, learn_rate, isFFT, savebest, makevis, sample):
+    recon_test(path_config, our_data2, our_recon_model, epoch, learn_rate, isFFT, savebest, makevis, sample)
+    #recon_test(path_config, nicu_data2, nicu_recon_model, epoch, learn_rate, isFFT, savebest, makevis, sample)
     
     epoch = 50
-    #class_train(path_config, name_ch, our_data1, our_recon_model, epoch, learn_rate, ["our01","our02","our03"])
+    class_train(path_config, name_ch, our_data1, our_recon_model, epoch, learn_rate, ["our01","our02","our03"])
     #class_train(path_config, name_ch, nicu_data1, nicu_recon_model, epoch, learn_rate, ["nicu01"])
     
-    #class_test1(path_config, our_data2, our_recon_model, epoch, learn_rate)
+    class_test1(path_config, our_data2, our_recon_model, epoch, learn_rate)
     #class_test1(path_config, nicu_data2, nicu_recon_model, epoch, learn_rate)
     
-    #class_test2(path_config, our_data2, our_recon_model, epoch, learn_rate)
+    class_test2(path_config, our_data2, our_recon_model, epoch, learn_rate)
     #class_test2(path_config, nicu_data2, nicu_recon_model, epoch, learn_rate)
-
-
-
-
 
